b11e5c570cb5472248ac2d557d96fad4651ae9ab
iqbal125 <iqbal125@yahoo.com>
2023-03-04 15:36:32 -0800

    add short selling

---
+++ Misc/ShortSelling.py
@@ -0,0 +1,25 @@
+# Long-Short static sample portfolio
+
+from AlgorithmImports import *
+
+# -------------------------------------------------------------------------------------------------------
+LONGS = ['TMV']; SHORTS = ['SQQQ']; LEV = 0.66; 
+# -------------------------------------------------------------------------------------------------------
+
+class PositiveMarSectorETF(QCAlgorithm):
+
+    def Initialize(self):
+        self.SetStartDate(2022, 1, 1)
+        self.SetCash(10000) 
+        res = Resolution.Minute
+        self.Longs = [self.AddEquity(ticker, res).Symbol for ticker in LONGS]
+        self.Shorts = [self.AddEquity(ticker, res).Symbol for ticker in SHORTS]
+        self.Schedule.On(self.DateRules.MonthStart(self.Longs[0]), self.TimeRules.AfterMarketOpen(self.Longs[0], 30), 
+            self.rebalance)
+
+    
+    def rebalance(self):
+        for sec in self.Longs:
+            self.SetHoldings(sec, len(self.Longs))                    
+        for sec in self.Shorts:
+            self.SetHoldings(sec, -0.5*LEV/len(self.Shorts))  
